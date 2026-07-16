b2213d3996838378e45b5682e064303f1713858d
Robert Edgar <robert@drive5.com>
2026-07-15 17:36:32 -0700

    daily_git

--- bash/search_reseek3_pdb_subsets_pdb.bash
+++ bash/search_reseek3_pdb_subsets_pdb.bash
@@ -2,8 +2,6 @@
 
 db=../big_reseek_dbs/pdb.bcb
 
-threads=64
-
 mkdir -p ../big_hits
 mkdir -p ../time
 mkdir -p ../logs
@@ -11,13 +9,12 @@ mkdir -p ../logs
 for N in 100 1000 # 10000
 do
 	q=../big_reseek_dbs/pdb_subset$N.bcb
-	name=reseek_search_pdbsubset${N}_afdb50_threads$threads
-	/bin/time -v -o ../time/$name.time \
-		reseek \
-			-flat_search_kappa $q \
-			-db $db \
-			-threads $threads \
-			-stats sf \
-			-output ../big_hits/$name.hits \
-			-log ../logs/$name.log
+	name=reseek_search_pdbsubset${N}_pdb
+	reseek \
+		-search $q \
+		-db $db \
+		-fast \
+		-stats sf \
+		-output ../big_hits/$name.hits \
+		-log ../logs/$name.log
 done
